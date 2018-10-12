2da415633d8e6282bd6971ef306bcafb810ce9c6
Marcio Mendes <mmendes.as@gmail.com>
2018-10-12 13:28:33 -0300

    adding state and props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
 
+import Table from './Table';
 class App extends Component {
+
+    state = {
+        characters: [
+            { 'name': 'Charlie', 'job': 'Janitor' },
+            { 'name': 'Mac', 'job': 'Bouncer' },
+            { 'name': 'Dee', 'job': 'Aspring actress' },
+            { 'name': 'Dennis', 'job': 'Bartender' }
+        ]
+    };
+
+    removeCharacter = index => {
+        const { characters } = this.state;
+
+        this.setState({
+            characters: characters.filter((character, i) => {
+                return i !== index;
+            })
+        });
+    }
+
     render() {
         return (
-            <div className="App">
-                <h1>Hello React</h1>
+            <div className="container">
+                <Table
+                    characterData={this.state.characters}
+                    removeCharacter={this.removeCharacter}
+                />
             </div>
         );
     }
