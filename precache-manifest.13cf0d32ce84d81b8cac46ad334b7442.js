19d4cc1bd5cd22f070a65e7e1f33f1af26ab3b69
Marcio Mendes <mmendes.as@gmail.com>
2018-10-12 15:23:20 -0300

    Updates

---
+++ precache-manifest.13cf0d32ce84d81b8cac46ad334b7442.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d5798adb5dcd6f950a8",
+    "url": "/mmendesas/react-tutorial/static/js/runtime~main.3d5798ad.js"
+  },
+  {
+    "revision": "6f0e6cff644e5673e1ab",
+    "url": "/mmendesas/react-tutorial/static/js/main.6f0e6cff.chunk.js"
+  },
+  {
+    "revision": "855276873adeae1ea0f1",
+    "url": "/mmendesas/react-tutorial/static/js/1.85527687.chunk.js"
+  },
+  {
+    "revision": "6f0e6cff644e5673e1ab",
+    "url": "/mmendesas/react-tutorial/static/css/main.b1bd169d.chunk.css"
+  },
+  {
+    "revision": "8b6edd6eb361b7a1d39c8611b8aa1139",
+    "url": "/mmendesas/react-tutorial/index.html"
+  }
+];
